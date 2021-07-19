cb6497320dd82148cc4e48900850f9e878f5ab80
Michał Warchoł <imsimilar@gmail.com>
2021-07-19 19:14:17 +0200

    :art: Added colors

--- src/theme.tsx
+++ src/theme.tsx
@@ -4,15 +4,23 @@ import { createBreakpoints } from '@chakra-ui/theme-tools'
 const fonts = { mono: `'Menlo', monospace` }
 
 const breakpoints = createBreakpoints({
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em',
+  sm: '576px',
+  md: '768px',
+  lg: '992px',
+  xl: '1200px',
+  max: '100em'
 })
 
 const theme = extendTheme({
   colors: {
-    black: '#16161D',
+    primary: '#18191A',
+    secondary: '#242526',
+    tertiary: '#3A3B3C',
+    hover: '#303031',
+    active: '#2D86FF',
+    textPrimary: '#D5D6DA',
+    textSecondary: "#ffffff",
+    buttonSuccess: '#42b72a',
   },
   fonts,
   breakpoints,
