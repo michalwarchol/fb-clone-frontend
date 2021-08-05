1fd2affbe14c5b99c1dd57f17af177195ab41d15
Michał Warchoł <imsimilar@gmail.com>
2021-08-05 23:17:29 +0200

    :soccer: Post activities

--- src/components/Activities.ts
+++ src/components/Activities.ts
@@ -1,1 +1,8 @@
-export default ["🎉 Celebrating", "👓 Watching", "🍴 Eating", "🍺 Drinking"];
+export default ["🎉 Celebrating", "📅 Attending", "🍴 Eating", "🍺 Drinking"];
+
+export const inDetail = {
+    Celebrating: ["😀 friendship", "🎂 a birthday", "👏 success", "✨ an anniversary"],
+    Attending: ["🎄 a Christmas party", "⛪ church", "🎫 an event", "🎉 a party"],
+    Eating: ["🍴 dinner", "🍽️ lunch", "🍕 pizza", "🍰 dessert"],
+    Drinking: ["☕ coffee", "🍺 beer", "🍵 tea", "🍷 wine"]
+}
