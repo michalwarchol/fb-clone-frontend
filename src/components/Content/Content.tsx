63b00a96e20399b0e20f407d4fdf5e11bf04e0ba
Michał Warchoł <imsimilar@gmail.com>
2021-08-08 22:26:10 +0200

    :unicorn: PostContainer component displayed

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,8 +1,14 @@
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Box, Grid, GridItem } from '@chakra-ui/react';
 import React from 'react'
+import { usePostsQuery } from '../../generated/graphql';
 import PostCreator from '../PostCreator/PostCreator';
+import PostContainer from "../Post/PostContainer";
 
 const Content:React.FC = () => {
+
+    const [{data}] = usePostsQuery({variables: {limit: 10}});
+    console.log(data)
+
     return(
         <Grid templateColumns="repeat(12, 1fr)" mt="20px">
             <GridItem colStart={1} colEnd={3}>
@@ -10,6 +16,15 @@ const Content:React.FC = () => {
             </GridItem>
             <GridItem colStart={4} colEnd={9}>
                 <PostCreator />
+                <Box>
+                    {!data ? (
+                    <div>loading</div>
+                    )
+                    :(
+                        data.posts.posts.map((post)=>(<PostContainer data={post}/>))
+                    )
+                    }
+                </Box>
             </GridItem>
         </Grid>
     )
