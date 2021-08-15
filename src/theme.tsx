a8bf5e6b67e1b10444aa836891dd608b8bba4a85
Michał Warchoł <imsimilar@gmail.com>
2021-08-15 19:37:53 +0200

    :art: Added reaction colors

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,29 +1,43 @@
-import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
+import { extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
 
-const fonts = { mono: `'Menlo', monospace` }
+const fonts = { mono: `'Menlo', monospace` };
 
 const breakpoints = createBreakpoints({
-  sm: '576px',
-  md: '768px',
-  lg: '992px',
-  xl: '1200px',
-  max: '100em'
-})
+  sm: "576px",
+  md: "768px",
+  lg: "992px",
+  xl: "1200px",
+  max: "100em",
+});
 
 const theme = extendTheme({
   colors: {
-    primary: '#18191A',
-    secondary: '#242526',
-    tertiary: '#303031',
-    hover: '#3A3B3C',
-    active: '#2D86FF',
-    textPrimary: '#D5D6DA',
+    primary: "#18191A",
+    secondary: "#242526",
+    tertiary: "#303031",
+    hover: "#3A3B3C",
+    active: "#2D86FF",
+    textPrimary: "#D5D6DA",
     textSecondary: "#ffffff",
-    buttonSuccess: '#42b72a',
+    buttonSuccess: "#42b72a",
+    pinkLove: "#FD637F",
+    yellowEmoji: "#FFE76D",
+    redAngry: "#E2410D"
   },
   fonts,
   breakpoints,
-})
+  components: {
+    Button: {
+      variants: {
+        basic: {
+          bg: "secondary",
+          _hover: { backgroundColor: "hover" },
+          _active: { backgroundColor: "hover" },
+        },
+      },
+    },
+  },
+});
 
-export default theme
+export default theme;
