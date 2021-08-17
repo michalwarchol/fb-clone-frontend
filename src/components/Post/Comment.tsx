8307272c3920157187ecdf01d993ee47a642c2f2
Michał Warchoł <imsimilar@gmail.com>
2021-08-17 23:45:56 +0200

    :card_index_dividers: Comment component

---
+++ src/components/Post/Comment.tsx
@@ -0,0 +1,21 @@
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+import React from "react";
+import { RegularCommentFragment } from "../../generated/graphql";
+
+interface Props {
+  data: RegularCommentFragment;
+}
+
+const CommentNote: React.FC<Props> = ({ data }) => {
+  return (
+    <Flex direction="row" mt="4px">
+      <Avatar size="sm" mr="10px" />
+      <Box bg="hover" borderRadius="18px" px="12px" py="8px">
+        <Text>{data.creator.username}</Text>
+        <Text wordBreak="break-word">{data.text}</Text>
+      </Box>
+    </Flex>
+  );
+};
+
+export default CommentNote;
