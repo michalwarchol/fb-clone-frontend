1eb052499173556325b13b49b413ea4832abc649
Michał Warchoł <imsimilar@gmail.com>
2021-08-29 17:27:55 +0200

    :wave: Created friend section

---
+++ src/components/ProfileBody/Friend.tsx
@@ -0,0 +1,35 @@
+import { Box, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import {
+  FriendRequestWithFriend,
+  useGetImageQuery,
+} from "../../generated/graphql";
+import NextLink from "next/link";
+
+interface Props {
+  data: FriendRequestWithFriend;
+}
+
+const Friend: React.FC<Props> = ({ data }) => {
+  const [{ data: image }] = useGetImageQuery({
+    variables: { imageId: data.friend.avatarId },
+    pause: !data,
+  });
+  return (
+    <NextLink href={"/profile?id=" + data.friend._id}>
+      <Box w="30%" mx="4px" cursor="pointer" mb="20px">
+        <Box w="100%px" h="100px" borderRadius="4px" overflow="hidden">
+          <Image
+            src={image?.getImage}
+            w="100%"
+            h="100%"
+            objectFit="cover"
+            fallbackSrc="https://gravatar.com/avatar/43484bed7620ffc1fec5d482af33bfae?s=400&d=mp&r=x"
+          />
+        </Box>
+        <Text>{data?.friend.username}</Text>
+      </Box>
+    </NextLink>
+  );
+};
+export default Friend;
---
+++ src/components/ProfileBody/FriendsSection.tsx
@@ -0,0 +1,37 @@
+import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
+import React from "react";
+import { useFriendCountQuery, useGetUserFriendRequestsQuery } from "../../generated/graphql";
+import Friend from "./Friend";
+
+interface Props {
+  id: number;
+  setActiveTab: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const FriendSection: React.FC<Props> = ({ id, setActiveTab }) => {
+  const [{ data: count }] = useFriendCountQuery({ variables: { userId: id } });
+  const [{data: friends}] = useGetUserFriendRequestsQuery({variables: {userId: id}});
+  
+  return (
+    <Box pt="20px" px="16px" bg="secondary" borderRadius="8px">
+      <Flex align="center" justify="space-between">
+        <Heading fontSize="22px" onClick={()=>setActiveTab(2)} _hover={{cursor: "pointer", textDecoration:"underline"}}>Friends</Heading>
+        <Button variant="basic" color="active" fontWeight="300" fontSize="18px" onClick={()=>setActiveTab(2)}>
+          See All Friends
+        </Button>
+      </Flex>
+      <Text fontSize="18px" color="gray">
+        {count?.friendCount} {count?.friendCount == 1 ? "friend" : "friends"}
+      </Text>
+      <Flex wrap="wrap" mt="20px" justify="space-around">
+          {
+              friends?.getUserFriendRequests.map((friend)=>(
+                  <Friend data={friend} key={friend.friend._id}/>
+              ))
+          }
+      </Flex>
+    </Box>
+  );
+};
+
+export default FriendSection;
