166ff0833e11695cf46f8b50d31d9ef9844b5e06
Michał Warchoł <imsimilar@gmail.com>
2021-08-31 00:17:14 +0200

    :zany_face: Created friends tab

---
+++ src/components/ProfileBody/FriendNote.tsx
@@ -0,0 +1,61 @@
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import {
+  FriendRequestWithFriend,
+  useGetImageQuery,
+} from "../../generated/graphql";
+import NextLink from "next/link";
+
+interface Props {
+  friendRequestWithFriend: FriendRequestWithFriend;
+}
+
+const FriendNote: React.FC<Props> = ({ friendRequestWithFriend }) => {
+  const [{ data: image }] = useGetImageQuery({
+    variables: { imageId: friendRequestWithFriend?.friend.avatarId },
+  });
+
+  return (
+    <Box
+      w="48%"
+      borderRadius="8px"
+      border="1px solid gray"
+      m="4px"
+      p="20px"
+    >
+      <Flex w="100%">
+        <Box
+          w={{ base: "36px", lg: "80px" }}
+          h={{ base: "36px", lg: "80px" }}
+          overflow="hidden"
+          borderRadius="8px"
+        >
+          <NextLink
+            href="/profile/[id]"
+            as={`/profile/${friendRequestWithFriend.friend._id}`}
+          >
+            <Image
+              src={image?.getImage}
+              w="100%"
+              h="100%"
+              objectFit="cover"
+              fallbackSrc="https://gravatar.com/avatar/43484bed7620ffc1fec5d482af33bfae?s=400&d=mp&r=x"
+              cursor="pointer"
+            />
+          </NextLink>
+        </Box>
+        <Flex ml="10px" align="center">
+          <NextLink
+            href="/profile/[id]"
+            as={`/profile/${friendRequestWithFriend.friend._id}`}
+          >
+            <Text fontSize="20px" fontWeight="bold" cursor="pointer">
+              {friendRequestWithFriend.friend.username}
+            </Text>
+          </NextLink>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
+export default FriendNote;
---
+++ src/components/ProfileBody/FriendsTab.tsx
@@ -0,0 +1,36 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
+import React from "react";
+import { useState } from "react";
+import { useGetUserFriendRequestsQuery } from "../../generated/graphql";
+import FriendNote from "./FriendNote";
+
+interface Props {
+  id: number;
+}
+
+const FriendsTab: React.FC<Props> = ({ id }) => {
+  const [skip, setSkip] = useState(0);
+  const limit = 10;
+  const [{ data }] = useGetUserFriendRequestsQuery({
+    variables: { userId: id, limit, skip },
+  });
+
+  return (
+    <Box w={{ base: "100%", lg: "876px" }} mt="16px" bg="secondary" borderRadius="8px">
+      <Box m="20px">
+        <Text fontSize="24px" fontWeight="bold">Friends</Text>
+      </Box>
+      <Box mx="auto">
+      <Flex mx="20px" direction="row" wrap="wrap" justify="space-between">
+        {data?.getUserFriendRequests.friendRequestsWithFriends.map((note) => (
+          <FriendNote friendRequestWithFriend={note} />
+        ))}
+      </Flex>
+      {
+          !data?.getUserFriendRequests.hasMore && <Text textAlign="center" fontSize="24px" fontWeight="bold">No more friends to show</Text>
+      }
+      </Box>
+    </Box>
+  );
+};
+export default FriendsTab;
