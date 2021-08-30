db0ffba5b8087396601c5ddec086b881537de0e7
Michał Warchoł <imsimilar@gmail.com>
2021-08-31 00:23:17 +0200

    :straight_ruler: Added min height

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -53,7 +53,7 @@ const Profile: NextPage<Props> = ({ id }) => {
 
   let body = <div></div>;
   if(data?.loggedUser){
-    body = <Box maxW="100vw" color="textPrimary" bg={"primary"}>
+    body = <Box maxW="100vw" color="textPrimary" bg={"primary"} minH="100vh">
     <Navbar loggedUser={data && data.loggedUser} />
     <Body
       editable={data.loggedUser.user._id == parseInt(id)}
