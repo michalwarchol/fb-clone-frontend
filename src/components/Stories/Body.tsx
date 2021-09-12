9a776174d650654b543ad9e66cb783509def501e
Michał Warchoł <imsimilar@gmail.com>
2021-09-12 22:35:50 +0200

    :bar_chart: Added progress bars of displayed stories

--- src/components/Stories/Body.tsx
+++ src/components/Stories/Body.tsx
@@ -1,8 +1,12 @@
-import { Flex } from "@chakra-ui/layout";
+import Icon from "@chakra-ui/icon";
+import { Flex, Text } from "@chakra-ui/layout";
 import React, { useState } from "react";
+import { IoMdPhotos } from "react-icons/Io";
 import { useGetRecentStoriesQuery } from "../../generated/graphql";
+import ImageStory from "./ImageStory";
 import StoriesBar from "./StoriesBar";
 import StoryDisplay from "./StoryDisplay";
+import TextStory from "./TextStory";
 
 interface Props {
   id?: number;
@@ -12,41 +16,107 @@ const Body: React.FC<Props> = ({ id }) => {
   const [activeUserStory, setActiveUserStory] = useState<number | null>(
     id ? id : null
   );
+  const [displayed, setDisplayed] = useState<number>(0);
   const [{ data }] = useGetRecentStoriesQuery();
   let seen = [];
-  let uniqueStories = data?.getRecentStories?.filter((story) => {
-    if (!(story.userId in seen)) {
-      seen[story.userId] = 1;
-      return true;
-    }
-    seen[story.userId] += 1;
-    return false;
-  });
-
-  const stories = uniqueStories?.map((elem) => {
-    return {
-      story: elem,
-      length: seen[elem.creator._id],
-    };
-  });
-
-  const storiesToDisplay = data?.getRecentStories.filter((story) => {
-    if (story.creator._id == activeUserStory) return true;
-    return false;
-  });
-
+  let stories = [];
+  let storiesToDisplay = [];
+  let userStories = [];
+  if(data){
+    let uniqueStories = data.getRecentStories?.filter((story) => {
+      if (!(story.userId in seen)) {
+        seen[story.userId] = 1;
+        return true;
+      }
+      seen[story.userId] += 1;
+      return false;
+    });
+  
+    stories = uniqueStories?.map((elem) => {
+      return {
+        story: elem,
+        length: seen[elem.creator._id],
+      };
+    });
+  
+    userStories = data.getRecentStories.filter((story) => {
+      if (story.creator._id == activeUserStory) return true;
+      return false;
+    });
+  
+    storiesToDisplay = userStories.map((story, index) => {
+      if (!!story.imageId) {
+        return <ImageStory imageId={story.imageId} key={index} />;
+      } else {
+        return (
+          <TextStory
+            text={story.text}
+            font={story.font}
+            gradient={story.gradient}
+            displayed={displayed}
+            setDisplayed={setDisplayed}
+            activeUserStory={activeUserStory}
+            setActiveUserStory={setActiveUserStory}
+            storiesLength={userStories.length}
+            time={userStories[displayed].time}
+            key={index}
+            nextActiveUser={
+              activeUserStory != null
+                ? parseInt(
+                    Object.keys(seen)[
+                      Object.keys(seen).indexOf(activeUserStory.toString()) + 1
+                    ]
+                  )
+                : null
+            }
+            previousActiveUser={
+              activeUserStory != null 
+              ? parseInt(Object.keys(seen)[Object.keys(seen).indexOf(activeUserStory.toString())-1])
+              :null
+            }
+          />
+        );
+      }
+    });
+  }
+  
+  console.log(storiesToDisplay);
   return (
     <Flex h="100vh">
       <StoriesBar
         activeUserStory={activeUserStory}
         setActiveUserStory={setActiveUserStory}
         stories={stories}
+        setDisplayed={setDisplayed}
       />
+      {data && activeUserStory 
+      ? 
       <StoryDisplay
-        activeUserStory={activeUserStory}
-        setActiveUserStory={setActiveUserStory}
-        stories={storiesToDisplay}
-      />
+      displayed={displayed}
+      setDisplayed={setDisplayed}
+      activeUserStory={activeUserStory}
+      setActiveUserStory={setActiveUserStory}
+      stories={storiesToDisplay}
+      time={userStories[displayed].time}
+      nextActiveUser={
+        activeUserStory != null
+          ? parseInt(
+              Object.keys(seen)[
+                Object.keys(seen).indexOf(activeUserStory.toString()) + 1
+              ]
+            )
+          : null
+      }
+    />
+    :
+    <Flex justify="center" align="center" direction="column" h="100%" w="100%" ml="360px">
+        <Icon as={IoMdPhotos} fontSize="100px" />
+        <Text fontWeight="bold" fontSize="22px">
+          Select a story to open.
+        </Text>
+      </Flex>
+    }
+      
     </Flex>
   );
 };
--- src/components/Stories/TextStory.tsx
+++ src/components/Stories/TextStory.tsx
@@ -1,26 +1,121 @@
-import { Flex, Text } from "@chakra-ui/layout";
-import React from "react";
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import React, { useEffect, useRef, useState } from "react";
+import { setStoriesInterval } from "../../utils/setStoriesInterval";
 
 interface Props {
   text: string;
   gradient: string;
   font: string;
+  activeUserStory: number | null;
+  setActiveUserStory: React.Dispatch<React.SetStateAction<number | null>>;
+  displayed: number;
+  setDisplayed: React.Dispatch<React.SetStateAction<number>>;
+  time: number;
+  storiesLength: number;
+  nextActiveUser: number;
+  previousActiveUser: number;
 }
 
-const TextStory: React.FC<Props> = ({ text, gradient, font }) => {
+const TextStory: React.FC<Props> = ({
+  text,
+  gradient,
+  font,
+  activeUserStory,
+  setActiveUserStory,
+  displayed,
+  setDisplayed,
+  time,
+  storiesLength,
+  nextActiveUser,
+  previousActiveUser,
+}) => {
+  const [passedTime, setPassedTime] = useState<number>(0);
+  let initialMount = useRef(true);
+
+  setStoriesInterval(
+    activeUserStory,
+    setActiveUserStory,
+    time,
+    displayed,
+    setPassedTime,
+    storiesLength,
+    setDisplayed,
+    initialMount,
+    nextActiveUser,
+    previousActiveUser
+  );
+
+  const makeScrubbers = (time: number, passed: number) => {
+    let scrubberWidth = Math.floor(100 / storiesLength);
+    let scrubbers = [];
+    for (let i = 0; i < storiesLength; i++) {
+      if (i < displayed) {
+        scrubbers.push(
+          <Box
+            w={scrubberWidth + "%"}
+            h="10px"
+            bg="rgba(255,255,255,0.3)"
+            key={i}
+          >
+            <Box bg="white" w="100%" h="100%"></Box>
+          </Box>
+        );
+      } else if (i > displayed) {
+        scrubbers.push(
+          <Box
+            w={scrubberWidth + "%"}
+            h="10px"
+            bg="rgba(255,255,255,0.3)"
+            key={i}
+          >
+            <Box bg="transparent" w="100%" h="100%"></Box>
+          </Box>
+        );
+      } else {
+        let width = (100 / time) * passed;
+        scrubbers.push(
+          <Box
+            w={scrubberWidth + "%"}
+            h="10px"
+            bg="rgba(255,255,255,0.3)"
+            key={i}
+          >
+            <Box bg="white" w={width + "%"} h="100%"></Box>
+          </Box>
+        );
+      }
+    }
+    return (
+      <Flex
+        w="100%"
+        justify="space-between"
+        position="absolute"
+        zIndex={99}
+        pt="68px"
+        top="0"
+      >
+        {scrubbers}
+      </Flex>
+    );
+  };
+
   return (
     <Flex
       align="center"
       justify="center"
       fontSize="26px"
-      w="505px"
-      h="96%"
+      w="100%"
+      h="100%"
       bg="red"
       fontFamily={font}
       bgGradient={gradient}
       borderRadius="10px"
+      position="relative"
     >
-      <Text textAlign="center">{text}</Text>
+      {makeScrubbers(time, passedTime)}
+      <Text textAlign="center" zIndex={98}>
+        {text}
+      </Text>
     </Flex>
   );
 };
