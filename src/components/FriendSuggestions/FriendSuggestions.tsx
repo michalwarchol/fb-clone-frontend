1ba88501230546adc731633e95a95da6ba38a573
Michał Warchoł <imsimilar@gmail.com>
2021-09-17 16:01:43 +0200

    :green_heart: Created friend suggestion components

---
+++ src/components/FriendSuggestions/FriendSuggestions.tsx
@@ -0,0 +1,78 @@
+import { IconButton } from "@chakra-ui/button";
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import React, { useState } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/Io";
+import { useGetSuggestedFriendsQuery } from "../../generated/graphql";
+import Suggestion from "./Suggestion";
+
+const FriendSuggestions: React.FC = () => {
+  const [slide, setSlide] = useState<number>(0);
+
+  const [{ data }] = useGetSuggestedFriendsQuery();
+  let a = 0;
+  if (data) {
+    a = data?.getSuggestedFriends.length - 4;
+  }
+  console.log(data)
+  return (
+    <Box
+      bg="secondary"
+      borderRadius="8px"
+      mb="20px"
+      position="relative"
+    >
+      {slide > 0 && (
+        <IconButton
+          aria-label="forward"
+          position="absolute"
+          icon={<IoIosArrowBack />}
+          variant="basic"
+          borderRadius="50%"
+          fontSize="30px"
+          w="60px"
+          h="60px"
+          left="-40px"
+          top="50%"
+          ml="10px"
+          zIndex="11"
+          onClick={() => setSlide(slide - 180)}
+        />
+      )}
+      {slide / 180 <= a && (
+        <IconButton
+          aria-label="forward"
+          position="absolute"
+          icon={<IoIosArrowForward />}
+          variant="basic"
+          borderRadius="50%"
+          fontSize="30px"
+          w="60px"
+          h="60px"
+          right="-30px"
+          top="50%"
+          ml="10px"
+          zIndex="11"
+          onClick={() => setSlide(slide + 180)}
+        />
+      )}
+      <Box mb="10px" p="10px">
+        <Text fontSize="24px" fontWeight="bold">
+          People You May Know
+        </Text>
+      </Box>
+      <Box overflow="hidden">
+        <Flex
+          wrap="nowrap"
+          zIndex="10"
+          transform={"translateX(" + -slide + "px)"}
+          transition="transform 0.6s ease-in-out"
+        >
+          {data?.getSuggestedFriends.map((friend, index) => (
+            <Suggestion friend={friend} key={index} />
+          ))}
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+export default FriendSuggestions;
---
+++ src/components/FriendSuggestions/Suggestion.tsx
@@ -0,0 +1,63 @@
+import { Button } from "@chakra-ui/button";
+import { Image } from "@chakra-ui/image";
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { useRouter } from "next/router";
+import React, { SyntheticEvent, useState } from "react";
+import { FaCheck, FaUserPlus } from "react-icons/fa";
+import { FriendSuggestion, useCreateFriendRequestMutation, useGetImageQuery } from "../../generated/graphql";
+
+interface Props {
+  friend: FriendSuggestion;
+}
+
+const Suggestion: React.FC<Props> = ({ friend }) => {
+  const [{ data: avatar }] = useGetImageQuery({
+    variables: { imageId: friend.friend.avatarId },
+  });
+  const [, createFriendRequest] = useCreateFriendRequestMutation();
+  const [requestSent, setRequestSent] = useState<boolean>(false);
+
+  const router = useRouter();
+
+  const goToProfile = () => {
+    router.push("/profile/"+friend.friend._id);
+  }
+
+  const sendRequest = async (e: SyntheticEvent) => {
+    setRequestSent(true);
+    e.stopPropagation();
+    await createFriendRequest({receiver: friend.friend._id});
+  }
+
+  return (
+    <Flex
+      p="10px"
+      w="180px"
+      borderRadius="8px"
+      cursor="pointer"
+      transition="background-color 0.2s ease-in-out"
+      _hover={{ backgroundColor: "tertiary" }}
+      onClick={goToProfile}
+    >
+      <Box w="100%">
+        <Box h="220px" w="160px" borderRadius="8px" overflow="hidden">
+          <Image
+            src={avatar?.getImage}
+            w="100%"
+            h="100%"
+            objectFit="cover"
+            fallbackSrc="https://gravatar.com/avatar/43484bed7620ffc1fec5d482af33bfae?s=400&d=mp&r=x"
+          />
+        </Box>
+        <Box>
+          <Text fontWeight="bold">{friend.friend.username}</Text>
+          <Text>{friend.mutual > 0 && friend.mutual + " mutual friends"}</Text>
+          <Button variant="basic" bg="tertiary" onClick={sendRequest} leftIcon={requestSent ? <FaCheck /> : <FaUserPlus />}>
+            {requestSent ? "Request sent" : "Send Request"}
+          </Button>
+        </Box>
+      </Box>
+    </Flex>
+  );
+};
+export default Suggestion;
