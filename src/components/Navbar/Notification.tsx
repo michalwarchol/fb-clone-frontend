1b26ce2680811f5235a08c02cd9336b1e06400ca
Michał Warchoł <imsimilar@gmail.com>
2021-09-21 20:41:39 +0200

    :bell: Created notifications

---
+++ src/components/Navbar/Notification.tsx
@@ -0,0 +1,42 @@
+import { Avatar } from '@chakra-ui/avatar';
+import Icon from '@chakra-ui/icon';
+import { Box, Flex, Text } from '@chakra-ui/layout';
+import React from 'react'
+import { FaFlag } from 'react-icons/fa';
+import { Notification, NotificationType, useGetImageQuery } from '../../generated/graphql';
+import { makeNotificationInfo } from '../../utils/makeNotificationInfo';
+import { parseAdvancedDate } from '../../utils/parseAdvancedDate';
+
+interface Props {
+    notification: Notification;
+}
+
+const NotificationNode:React.FC<Props> = ({notification}) => {
+    const [{data: avatar}] = useGetImageQuery({variables: {imageId: notification.triggerUser.avatarId}});
+
+    let image = <Icon as={FaFlag} bg="active" w="30px" h="30px" />;
+    if(notification.type != NotificationType.Info){
+        image=<Avatar src={avatar?.getImage} size="lg" />
+    }
+
+    return(
+        <Box 
+        p="8px"
+        borderRadius="8px"
+        _hover={{backgroundColor: "hover"}}>
+            <Flex>
+                <Box mr="10px">
+                    <Flex justify="center" align="center" w="60px" h="60px" bg="active" borderRadius="50%">
+                    {image}
+                    </Flex>
+                    
+                </Box>
+                <Box>
+                    <Text>{makeNotificationInfo(notification)}</Text>
+                    <Text color="active">{parseAdvancedDate(notification.createdAt)}</Text>
+                </Box>
+            </Flex>
+        </Box>
+    )
+}
+export default NotificationNode;
--- src/components/Navbar/Options.tsx
+++ src/components/Navbar/Options.tsx
@@ -8,21 +8,39 @@ import {
   MenuList,
   Text,
   Avatar,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  PopoverBody,
+  PopoverHeader,
+  Badge,
+  Box,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { TiArrowSortedDown } from "react-icons/ti";
 import { ImExit } from "react-icons/im";
-import { FullUser, useLogoutMutation } from "../../generated/graphql";
+import {
+  FullUser,
+  useGetNewNotificationsCountQuery,
+  useGetUserNotificationsQuery,
+  useLogoutMutation,
+} from "../../generated/graphql";
 import { useRouter } from "next/dist/client/router";
 import NextLink from "next/link";
+import { MdNotificationsActive } from "react-icons/md";
+import NotificationNode from "./Notification";
 
 interface Props {
   loggedUser: FullUser;
 }
 
 const Options: React.FC<Props> = ({ loggedUser }) => {
+  const [{ data }] = useGetUserNotificationsQuery();
+  const [{ data: count }] = useGetNewNotificationsCountQuery();
   const [, logout] = useLogoutMutation();
   const router = useRouter();
+  const [notsOpened, setNotsOpened] = useState(false);
+
   return (
     <Flex align="center">
       <NextLink href={"/profile/" + loggedUser.user._id}>
@@ -30,7 +48,7 @@ const Options: React.FC<Props> = ({ loggedUser }) => {
           display={{ base: "none", md: "flex" }}
           direction="row"
           align="center"
-          mr="20px"
+          mr="10px"
           px="8px"
           py="4px"
           borderRadius="20px"
@@ -61,6 +79,63 @@ const Options: React.FC<Props> = ({ loggedUser }) => {
           <Text fontWeight="bold">{loggedUser?.user.username}</Text>
         </Flex>
       </NextLink>
+      <Popover closeOnEsc closeOnBlur>
+        <PopoverTrigger>
+          <Box position="relative">
+            <IconButton
+              aria-label="notifications"
+              borderRadius="50%"
+              boxSizing="border-box"
+              bg={"tertiary"}
+              icon={<MdNotificationsActive />}
+              fontSize="24px"
+              mr="10px"
+              _hover={{
+                bg: "hover",
+              }}
+              _active={{
+                filter: "brightness(0.7)",
+                color: "active",
+                bg: "blue.700",
+              }}
+              onClick={() => setNotsOpened(true)}
+            />
+            {count && count.getNewNotificationsCount > 0 && !notsOpened && (
+              <Badge
+                position="absolute"
+                bottom="0"
+                left="30px"
+                bg="red.500"
+                color="textPrimary"
+                borderRadius="50%"
+                cursor="pointer"
+              >
+                {count.getNewNotificationsCount}
+              </Badge>
+            )}
+          </Box>
+        </PopoverTrigger>
+        <PopoverContent
+          bg="secondary"
+          border="none"
+          borderRadius="8px"
+          w="360px"
+        >
+          <PopoverHeader
+            border="none"
+            fontSize="28px"
+            fontWeight="bold"
+            color="textSecondary"
+          >
+            Notifications
+          </PopoverHeader>
+          <PopoverBody bg="secondary" borderRadius="8px">
+            {data?.getUserNotifications.map((notification, index) => (
+              <NotificationNode notification={notification} key={index} />
+            ))}
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
       <Menu>
         <MenuButton
           as={IconButton}
@@ -76,7 +151,7 @@ const Options: React.FC<Props> = ({ loggedUser }) => {
             color: "active",
             bg: "blue.700",
           }}
-        ></MenuButton>
+        />
         <MenuList w="360px" px="8px" bg="secondary" border={0}>
           <MenuItem>drugi</MenuItem>
           <MenuItem
