d7aed6bf1624e0a5362983fbd144a14b21428112
Michał Warchoł <imsimilar@gmail.com>
2021-12-07 18:22:25 +0100

    :label: Added title tag in Head of the _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,20 @@
 import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import theme from "../theme";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
+      <>
+      <Head>
+        <title>Clone-book</title>
+      </Head>
       <ChakraProvider theme={theme}>
         <CSSReset />
         <Component {...pageProps} />
       </ChakraProvider>
+      </>
   );
 }
 
